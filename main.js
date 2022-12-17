17c1e409ca71f7e8ccb3ed6d8482ed5fef3357ec
praneil231 <84107249+praneil231@users.noreply.github.com>
2022-12-17 15:48:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+song = "";
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    Image(video, 0, 0, 600, 500);
+}
+function play()
+{
+    song.play();
+}
+function preload(){
+    song = loadSound("music.mp3");
+}
